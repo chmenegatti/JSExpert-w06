d839036d4682215ef77ab1c09f5d0e7326d37ecf
Cesar Menegatti <cesar.menegatti@bitfy.app>
2022-03-16 18:37:26 -0300

    Initial Routes, Controllers, Services created. Jest installed and Configured

---
+++ server/config.js
@@ -0,0 +1,33 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const root = join(currentDir, '../');
+
+const audioDirecory = join(root, 'audio');
+const publicDirectory = join(root, 'public');
+
+export default {
+  port: process.env.PORT || 3000,
+  dir: {
+    root,
+    audioDirecory,
+    publicDirectory,
+    songsDirectory: join(audioDirecory, 'songs'),
+    fxDirectory: join(audioDirecory, 'fx'),
+  },
+  pages: {
+    homeHTML: 'home/index.html',
+    controller: 'controller/index.html',
+  },
+  location: {
+    home: '/home',
+  },
+  constants: {
+    CONTENT_TYPE: {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+    }
+  }
+}
---
+++ tests/unit/server/routes.test.js
@@ -0,0 +1,8 @@
+import { jest, expect, describe, test } from '@jest/globals';
+
+describe('#Routes', () => {
+  test('my first test', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
